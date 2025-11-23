dbc6633aaa2225f538771f86e9509081b03a9272
Akash <64306015+therealakash@users.noreply.github.com>
2025-11-23 17:42:00 +0530

    Update checksorted.java
    final correct code

--- checksorted.java
+++ checksorted.java
@@ -1,21 +1,31 @@
 
 //. Check if an array is sorted.
+
+import java.util.Scanner;
+
 class checksorted{
      static boolean checksorted(int[] arr){
-        boolean check=false;
-        for(int i =0;i<arr.length;i++){
-                if(arr[i]<=arr[i+1]){
-                    continue;
-                    check=true;
+        for(int i =0;i<arr.length-1;i++){
+                if(arr[i]>arr[i+1]){
+                 
+                       return false;
                 }
-                else{
-                    break;
-                }
+             
         }
-        return  check;
+        return  true;
      }
 
     public static void main(String[] args) {
         
+           Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array:");
+        int s=sc.nextInt();
+        int [] arr=new int[s];
+        for(int i =0;i<s;i++){
+            System.out.println("enter values for array:");
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("the array is sorted :"+checksorted(arr));
+            
     }
 }
